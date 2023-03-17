02d69012add928c476efcc74af7dd5420d3a1a18
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-17 16:43:08 +0100

    add RectangleRunner for check code

---
+++ src/RectangleRunner.java
@@ -0,0 +1,11 @@
+public class RectangleRunner {
+  public static void main(String[] args) {
+    Rectangle rectangle1 = new Rectangle(20,30);
+    Rectangle rectangle2 = new Rectangle(30, 20);
+    Rectangle rectangle3 = new Rectangle(35, 20);
+    Rectangle rectangle4 = new Rectangle(20, 30);
+    System.out.println("r1=r2? "+rectangle1.equals(rectangle2));
+    System.out.println("r2=r3? "+rectangle2.equals(rectangle3));
+    System.out.println("r1=r4? "+rectangle1.equals(rectangle4));
+  }
+}
